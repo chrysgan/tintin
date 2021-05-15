4c4ffd3dac97fbf33ab9e4374c489f78b6c5611f
chrysgan <chrysgan@gmail.com>
2021-05-15 16:28:28 +0200

    Mis à jour affichage dernière figurine

--- app/mvc/controllers/home.php
+++ app/mvc/controllers/home.php
@@ -2,12 +2,8 @@
 require_once DIR_MODELS.$controller.'.php';
 
 
-if($objet['serid']==null){
-    $urlending="/".$objet['typecode']."/".$objet['ediid'];
-}
-else{
-    $urlending = "/series/".$objet['serid'];
-}
+$urlending="/editeurs/".$objet['ediid']."#".$objet['objid'];
+
 $url =WEBROOT.$page_galleries.$urlending;
 $libelle_objet=$objet['objnom'];
 
--- tools/todo.php
+++ tools/todo.php
@@ -1,2 +1,4 @@
 <?php
 // TODO: mettre des meta mots dans les pages
+// TODO: <meta name="description" content="Free web tutorials"> in head  description ds recheche google max 130
+// TODO: <meta name="title" content="Free web tutorials"> in head gros titre bleu dans recherche google max 60
