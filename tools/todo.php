db7b85119fc09cd688b0d5110d5a75a056ee3700
chrysgan <chrysgan@gmail.com>
2021-11-01 22:16:58 +0100

    modif css pour affichage image sur telephone mobile

--- tools/todo.php
+++ tools/todo.php
@@ -2,3 +2,6 @@
 // TODO: mettre des meta mots dans les pages
 // TODO: <meta name="description" content="Free web tutorials"> in head  description ds recheche google max 130
 // TODO: <meta name="title" content="Free web tutorials"> in head gros titre bleu dans recherche google max 60
+// TODO: sur les vignettes l'info bulle n'affiche pas le nom complet de la figurine
+// TODO: sur la vignette au lieu de mettre 0/1 pour possédé mettre des couleurs
+// TODO: espace manquant sur les vignettes entre le mois et l'année
