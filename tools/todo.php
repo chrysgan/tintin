cc68998751ed8940d3c867dd743790e69c5a34d4
chrysgan <chrysgan@gmail.com>
2021-11-21 13:03:52 +0100

    [TODO] maj

--- tools/todo.php
+++ tools/todo.php
@@ -1,4 +1,5 @@
 <?php
-// TODO: mettre des meta mots dans les pages
-// TODO: <meta name="description" content="Free web tutorials"> in head  description ds recheche google max 130
-// TODO: <meta name="title" content="Free web tutorials"> in head gros titre bleu dans recherche google max 60
+// TODO: <meta name="description" content="résumé concis de la page web"> : max ! 155carac utilisé comme libellé pour les favoris
+// TODO: mettre un title plus long pour avoir une meilleure description dans les moteurs de recherche
+// TODO: creer le module envoi de news letter avec la fonction ne plus souscrire à la newsletter dans le mail
+// TODO: ajouter la fonction supprimer mon compte
